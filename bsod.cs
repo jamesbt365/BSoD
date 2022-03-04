757b51ea7a43e217d55593fb1e716383079ebdc3
jamesbt365 <jamesbt365@gmail.com>
2022-03-04 21:03:07 +0000

    Initial Commit

---
+++ bsod.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using OpenTabletDriver.Plugin;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+
+namespace bsod
+{
+    [PluginName("Bsod Binding"), SupportedPlatform(PluginPlatform.Windows)]
+    public unsafe class Bsod : IStateBinding
+    {
+        [DllImport("ntdll.dll")]
+        public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+
+        [DllImport("ntdll.dll")]
+        public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+
+       public void Press(TabletReference reference, IDeviceReport report)
+        {
+        }
+
+        public void Release(TabletReference reference, IDeviceReport report)
+        {
+            {
+                Boolean t1;
+                uint t2;
+                RtlAdjustPrivilege(19, true, false, out t1);
+                NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out t2);
+            }
+        }
+    }
+}
